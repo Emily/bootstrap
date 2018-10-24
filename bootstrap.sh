9a76080c7179b616bcf0cd4ba6f514910cd7dec5
Emily Dobervich <emily@dobervich.com>
2018-10-24 16:38:02 -0700

    update with additional nice things

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
+shopt -s extdebug
 
 cd ~
+
+brewList=""
 
 echoGreen() {
   echo -e \\033[32m$1\\033[0m
 }
 
 gitClone() {
-  repo=$(basename /$1)
-  if [ -d ~/$repo ]
+  local arity="$#"
+
+  if [ "${arity}" == "2" ]
+  then
+    local destDir=$2
+  else
+    local destDir=$(basename /$1)
+  fi
+
+  if [ -d $destDir ]
   then
     echoGreen "$1 already cloned"
   else
-    echo "cloning $1..."
-    git clone git@github.com:$1 &> /dev/null
+    if [ "${arity}" == "2" ]
+    then
+      echo "cloning $1 to $destDir..."
+      git clone git@github.com:$1 $destDir &> /dev/null
+    else
+      echo "cloning $1..."
+      git clone git@github.com:$1 &> /dev/null
+    fi
   fi
 }
 
@@ -31,12 +48,31 @@ finstall() {
   fi
 }
 
+initBrewList() {
+  brewList=$(brew list)
+  brewTaps=$(brew tap)
+}
+
+checkBrewList() {
+  split=$(basename $1)
+  for item in $brewList
+  do
+    if [ "$split" == "$item" ]
+    then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
 brew_install() {
-  brew list $1 &> /dev/null
+  checkBrewList $1
 
   if [ $? -ne 0 ]
   then
-    finstall "brew install $1" $1
+    echo "$1 not installed"
+    # finstall "brew install $1" $1
   else
     echoGreen "$1 already installed"
   fi
@@ -53,9 +89,21 @@ cask_install() {
   fi
 }
 
+checkBrewTaps() {
+  for item in $brewTaps
+  do
+    if [ "$1" == "$item" ]
+    then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
 brew_tap() {
-  brew tap | grep $1 &> /dev/null
-  
+  checkBrewTaps $1
+
   if [ $? -ne 0 ]
   then
     echo "tapping $1"
@@ -93,6 +141,8 @@ do
   echo "please upload public key to github (and then press return)"
   read -n 1
 done
+
+initBrewList
 
 brew_install chruby
 brew_install ruby-install
@@ -143,8 +193,26 @@ then
   echo "cloning dotfiles"
   homesick clone git@github.com:emily/dotfiles &> /dev/null
   homesick link dotfiles &> /dev/null
+  pushd "$HOME/.vim/bundle/vimproc.vim" &> /dev/null
+  make &> /dev/null
+  popd &> /dev/null
 else
   echoGreen "dotfiles already cloned"
 fi
 
 gitClone "mbadolato/iTerm2-Color-Schemes"
+gitClone "creationix/nvm" "$HOME/.nvm"
+
+pushd "$HOME/.nvm" &> /dev/null
+git checkout "v0.33.11" &> /dev/null
+source nvm.sh
+popd &> /dev/null
+
+which node &> /dev/null
+if [ $? -ne 0 ]
+then
+  echo "installing node..."
+  nvm install node
+else
+  echoGreen "node already installed"
+fi
